91b7add6300bddd3855d4f20938c6f62019f8403
Vijay <vbvijay84@gamil.com>
2024-02-18 22:56:41 +0530

    mongo chnges

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,13 +3,13 @@
 import { revalidatePath } from "next/cache";
 
 import User from "../database/models/user.model";
-import { connectDatabase } from "../database/mongoose";
+import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
 // CREATE
 export async function createUser(user: CreateUserParams) {
   try {
-    await connectDatabase();
+    await connectToDatabase();
 
     const newUser = await User.create(user);
 
@@ -22,7 +22,7 @@ export async function createUser(user: CreateUserParams) {
 // READ
 export async function getUserById(userId: string) {
   try {
-    await connectDatabase();
+    await connectToDatabase();
 
     const user = await User.findOne({ clerkId: userId });
 
@@ -37,7 +37,7 @@ export async function getUserById(userId: string) {
 // UPDATE
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
-    await connectDatabase();
+    await connectToDatabase();
 
     const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
       new: true,
@@ -54,7 +54,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
 // DELETE
 export async function deleteUser(clerkId: string) {
   try {
-    await connectDatabase();
+    await connectToDatabase();
 
     // Find user to delete
     const userToDelete = await User.findOne({ clerkId });
@@ -76,7 +76,7 @@ export async function deleteUser(clerkId: string) {
 // USE CREDITS
 export async function updateCredits(userId: string, creditFee: number) {
   try {
-    await connectDatabase();
+    await connectToDatabase();
     const updatedUserCredits = await User.findOneAndUpdate(
       { _id: userId },
       { $inc: { creditBalance: creditFee } },
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,29 +1,30 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGO_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-let cached : MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose;
 
-if(!cached){
+if (!cached) {
   cached = (global as any).mongoose = {
-    conn: null, promise: null
-  }
+    conn: null,
+    promise: null,
+  };
 }
 
-
-export const connectDatabase = async () => {
-  if(cached.conn) return cached.conn
-  if(!MONGODB_URL) throw new Error("Missing MONGODB_URL")
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "imaginify",
+      bufferCommands: false,
+    });
   cached.conn = await cached.promise;
   return cached.conn;
-}
+};
